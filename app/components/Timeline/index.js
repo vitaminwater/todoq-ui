4c9a0656e9c45075ddcf5cbbd6fbba503b00d7dd
vitaminwater <stantin04@gmail.com>
2017-03-21 22:55:28 +0100

    styled log and timeline output

--- app/components/Timeline/index.js
+++ app/components/Timeline/index.js
@@ -4,12 +4,41 @@
 *
 */
 
+import styled from 'styled-components';
 import React, { PropTypes } from 'react';
-
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-import { List, Item } from 'components/UIKit/list';
+import { List } from 'components/UIKit/list';
+
+export const Item = styled.div`
+  background-color: white;
+  margin-bottom: 10pt;
+`;
+
+const Container = styled.div`
+  position: relative;
+  padding: 40pt 5pt 40pt 60pt;
+  background-color: ${props => props.color};
+  border: 1px solid #afafaf;
+  border-right: none;
+  margin-left: 30pt;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  top: 50%; left: 10pt;
+  transform: translate(0, -50%);
+  border-radius: 20pt;
+  width: 40pt;
+  height: 40pt;
+  background-color: white;
+`;
+
+const Alt = styled.span`
+  font-size: 0.9em;
+  color: #555555;
+`;
 
 class Timeline extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -17,13 +46,18 @@ class Timeline extends React.PureComponent { // eslint-disable-line react/prefer
     return (
       <List>
         {
-          activities.map((activity) => {
-            return (
+          activities.map((activity) => (
               <Item onClick={this._handleOpen(activity)} key={activity.get('id')}>
-                {activity.get('name')}
+                <Container color={activity.get('color')}>
+                  <Image src={`http://localhost:8000${activity.get('image')}`} />
+                  {activity.get('name')}<br />
+                  {activity.get('why').split('\n').map((item, key) => (
+                    <Alt key={key}>{item}<br/></Alt>
+                  ))}
+                </Container>
               </Item>
-            );
-          })
+            )
+          )
         }
       </List>
     );
--- app/containers/ActivityLog/index.js
+++ app/containers/ActivityLog/index.js
@@ -7,11 +7,16 @@
 import styled from 'styled-components';
 import { fromJS } from 'immutable';
 import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import makeSelectActivityLog from './selectors';
 import messages from './messages';
+import moment from 'moment';
+
+import note from './img/note.png';
+import todo from './img/todo.png';
 
 import { loadMoreLogs, createLog, reset } from './actions';
 import { logsSelector } from './selectors';
@@ -31,6 +36,24 @@ const LogsContainer = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column-reverse;
+  overflow-y: auto;
+`;
+
+const Log = styled.div`
+  position: relative;
+  padding: 30pt 10pt 30pt 10pt;
+  ${props => props.first ? '' : 'border-bottom: 2px dashed #E0E0E0;'}
+`;
+
+const LogIcon = styled.img`
+  margin: 20pt;
+  height: 40pt;
+`;
+
+const LogDate = styled.div`
+  position: absolute;
+  top: 5pt; left: 5pt;
+  color: #a0a0a0;
 `;
 
 const InputContainer = styled.div`
@@ -77,10 +100,16 @@ export class ActivityLog extends React.PureComponent { // eslint-disable-line re
   }
 
   componentWillReceiveProps(nextProps) {
-    const { activityId } = this.props.params;
-    const { activityId: nextActivityId } = nextProps.params;
+    const { params: { activityId }, logs } = this.props;
+    const { params: { activityId: nextActivityId }, logs: nextLogs } = nextProps;
     if (activityId != nextActivityId) {
       this._handleLoadLogs(nextActivityId);
+    }
+    if (logs.size && nextLogs.size && (logs.get('0').get('id') != nextLogs.get('0').get('id'))) {
+      setTimeout(() => {
+        const elem = ReactDOM.findDOMNode(this.refs.logScroll);
+        elem.scrollTop = elem.scrollHeight;
+      }, 30);
     }
   }
 
@@ -88,12 +117,16 @@ export class ActivityLog extends React.PureComponent { // eslint-disable-line re
     const { logs } = this.props;
     return (
       <Container>
-        <LogsContainer>
+        <LogsContainer ref='logScroll'>
           {
-            logs.map(log => (
-              <div key={log.get('id')}>
+            logs.map((log, i) => (
+              <Log first={i == 0} key={log.get('id')}>
+                <LogDate>
+                  {moment(log.get('inserted_at')).format('MMMM Do YYYY, h:mm:ss a')}
+                </LogDate>
+                <LogIcon src={note} />
                 {log.get('text')}
-              </div>
+              </Log>
             ))
           }
         </LogsContainer>
